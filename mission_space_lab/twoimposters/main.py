c4782fb8a9f5059f6739e90d2cd391d35070a1bb
Daniel Murray <daniel.murray@stjosephsrush.ie>
2021-02-19 22:37:40 +0000

    Update main.py

--- mission_space_lab/twoimposters/main.py
+++ mission_space_lab/twoimposters/main.py
@@ -20,8 +20,8 @@ logfile(dir_path/"2 Imposters.log")
 
 # Where the ISS location is at the moment
 name = "ISS (ZARYA)"
-line1 = "1 25544U 98067A   20316.41516162  .00001589  00000+0  36499-4 0  9995"
-line2 = "2 25544  51.6454 339.9628 0001882  94.8340 265.2864 15.49409479254842"
+line1 = "1 25544U 98067A   21050.35666428  .00001943  00000-0  43448-4 0  9992"
+line2 = "2 25544  51.6441 205.5251 0003032  33.1814  49.2099 15.48980511270331"
 iss = readtle(name, line1, line2)
 
 # Set up your camera
