077917248bbfe947e3d4b7a30ff6879b7fdde937
Daniel Murray <daniel.murray@stjosephsrush.ie>
2022-02-24 21:21:09 +0000

    Update bronze.py

--- 2022 MSL Code/bronze.py
+++ 2022 MSL Code/bronze.py
@@ -1,3 +1,5 @@
+#Team Bronze
+
 from pathlib import Path
 from logzero import logger, logfile
 from sense_hat import SenseHat
@@ -15,7 +17,7 @@ __________
  |______  / |__|   \____/|___|  /_____ \\___  >
         \/                    \/      \/    \/ 
 Team Bronze
-Classifying cloud types using ML using data obtained from snapping the earth for 3 hours.
+Classifying cloud types using ML using data obtained from snapping the earth for 3 hours (Fully Tested!)
 Abigail Bosch & Stephen Flynn
 St Josephs Rush, Dublin, Ireland, Planet Earth, Milky Way Galaxy
 2022
@@ -36,7 +38,7 @@ Bridie, Rose, thank you!
 Stephen was here.
 
 
-"I serisously can't beat that wth...Like I made a Jeff Bezos joke and you did a poem lmao" - Stephens message to Abigail after comparing our messages.
+"I serisously can't beat that with...Like I made a Jeff Bezos joke and you did a poem lmao" - Stephens message to Abigail after comparing our messages.
 
 '''
 
@@ -55,19 +57,14 @@ def plus_data_from_csv(data_stored_directory, data):
 
 def calculate_angle(angle):
     """
-    calculate_angle a `skyfield` Angle to an EXIF-appropriate
-    representation (rationals)
-    e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
-
-    Return a tuple containing a boolean and the calculate_angle,
-    with the boolean indicating if the angle is negative.
+    Work out a `skyfield` Angle and make it EXIF-appropriate
     """
     sign, degrees, minutes, seconds = angle.signed_dms()
     exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return sign < 0, exif_angle
 
-def take_picture(camera, image):
-    """Use `camera` to take_picture an `image` file with lat/long EXIF data."""
+def capture(camera, image):
+    """Use `camera` to capture an `image` file with lat/long EXIF data."""
     location = ISS.coordinates()
 
     # calculate_angle the latitude and longitude to EXIF-appropriate representations
@@ -80,8 +77,8 @@ def take_picture(camera, image):
     camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
     camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
 
-    # take_picture the image
-    camera.take_picture(image)
+    # capture the image
+    camera.capture(image)
 
 
 base_folder = Path(__file__).parent.resolve()
@@ -94,7 +91,7 @@ sense = SenseHat()
 
 # Set up camera
 cam = PiCamera()
-cam.resolution = (1296, 972)
+cam.resolution = (4056, 3040) # Full throttle! (Don't worry, testing came out <3GB)
 
 # Initialise the CSV file
 data_stored_directory = base_folder/"data.csv"
@@ -106,7 +103,7 @@ counter = 1
 begin_experiment = datetime.now()
 current_time = datetime.now()
 # Run a loop for (almost) three hours
-while (current_time < begin_experiment + timedelta(minutes=178)):
+while (current_time < begin_experiment + timedelta(minutes=161)): # 161 as 178 sometimes went over in testing
     try:
         humidity = round(sense.humidity, 4)
         temperature = round(sense.temperature, 4)
@@ -122,14 +119,15 @@ while (current_time < begin_experiment + timedelta(minutes=178)):
             humidity,
         )
         plus_data_from_csv(data_stored_directory, data)
-        # take_picture image
+        # capture image
         image_file = f"{base_folder}/photo_{counter:03d}.jpg"
-        take_picture(cam, image_file)
+        capture(cam, image_file)
         # Log event
-        logger.info(f"iteration {counter}")
+        logger.info(f" is on iteration {counter} of about 470 or so. All good.")
         counter += 1
-        sleep(30)
+        sleep(20) # 20 as opposed to 30 or 60. This number came from hours of testing in bright light.
         # Update the current time
         current_time = datetime.now()
     except Exception as e:
         logger.error(f'{e.__class__.__name__}: {e}')
+
